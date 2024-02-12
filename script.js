0a4f17fcce05e1e0de6772f4ff06399920e92ec4
Matthew Tseng <mthwtsng@gmail.com>
2024-02-11 17:08:02 -0800

    Nav bar changes

---
+++ script.js
@@ -0,0 +1,19 @@
+// Array of dynamic texts to display
+var dynamicTexts = ["Computer Science Student", "Software Engineer", "Golfer"];
+
+// Index to keep track of the current dynamic text
+var dynamicTextIndex = 1;
+
+// Function to change the dynamic part of the text with a fading effect
+function changeDynamicTextWithFade() {
+    var dynamicText = document.getElementById("dynamic-text");
+    dynamicText.style.opacity = 0; // Start fading out the current text
+    setTimeout(function() {
+        dynamicText.textContent = dynamicTexts[dynamicTextIndex];
+        dynamicText.style.opacity = 1; // Start fading in the new text
+        dynamicTextIndex = (dynamicTextIndex + 1) % dynamicTexts.length; // Loop back to the beginning if end is reached
+    }, 300); // Wait for 500ms (half a second) before changing the text
+}
+
+// Change the dynamic part of the text with a fading effect every 3 seconds
+setInterval(changeDynamicTextWithFade, 3000);

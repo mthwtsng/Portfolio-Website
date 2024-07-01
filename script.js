c332c4769a7169703d3b79582635deda76bb9871
Matt T <mthwtsng@gmail.com>
2024-07-01 00:41:15 -0700

    Added content to Portfolio + Resume sections

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-// Array of dynamic texts to display
+
 var dynamicTexts = ["Computer Science Student", "Golfer", "Canucks Fan", "Dog Lover"];
 
-// Index to keep track of the current dynamic text
+
 var dynamicTextIndex = 1;
 
-// Function to change the dynamic part of the text with a fading effect
+
 function changeDynamicTextWithFade() {
     var dynamicText = document.getElementById("dynamic-text");
-    dynamicText.style.opacity = 0; // Start fading out the current text
+    dynamicText.style.opacity = 0; 
     setTimeout(function() {
         dynamicText.textContent = dynamicTexts[dynamicTextIndex];
-        dynamicText.style.opacity = 1; // Start fading in the new text
-        dynamicTextIndex = (dynamicTextIndex + 1) % dynamicTexts.length; // Loop back to the beginning if end is reached
-    }, 300); // Wait for 500ms (half a second) before changing the text
+        dynamicText.style.opacity = 1; 
+        dynamicTextIndex = (dynamicTextIndex + 1) % dynamicTexts.length; 
+    }, 300);
 }
 
-// Change the dynamic part of the text with a fading effect every 3 seconds
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
 setInterval(changeDynamicTextWithFade, 2000);
 
 
